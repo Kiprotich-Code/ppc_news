53ad6bcab833465a0fca77044c7146b74ac4c898
Kiprotich-Code <fleeklawi@gmail.com>
2025-07-26 23:45:40 +0300

    Remove escape linting to enable linting during builds

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,6 @@ const nextConfig: NextConfig = {
     styledComponents: true,
     reactRemoveProperties: process.env.NODE_ENV === 'production', // Remove data-testids in prod
   },
-  
-  // Environment-aware settings
-  eslint: {
-    ignoreDuringBuilds: !!process.env.SKIP_LINT,
-  },
 }
 
 export default nextConfig
